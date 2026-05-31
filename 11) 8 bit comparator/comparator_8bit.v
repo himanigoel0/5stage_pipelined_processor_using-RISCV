4d5bc7e46e2088bddd092498b51d12deaae79fa9
himanigoel0 <24110134@iitgn.ac.in>
2026-05-31 21:49:19 +0530

    Add files via upload

--- 11) 8 bit comparator/comparator_8bit.v
+++ 11) 8 bit comparator/comparator_8bit.v
@@ -1,27 +1,59 @@
 `timescale 1ns / 1ps
 
-module comparator_8bit(
-    input [7:0] A, B,
-    output reg gt, et, lt
+module comparator_8bitt(
+    input signed [7:0] A, B,
+    output reg et, lt, gt
 );
 
     always @(*) begin
-        if (A > B) begin
-            gt = 1; 
-            lt = 0; 
+        if (A[7] == 0 && B[7] == 0) begin   
+            if (A > B) begin
+                gt = 1; 
+                lt = 0;
+                et = 0;
+            end
+            else if (A < B) begin
+                gt = 0; 
+                lt = 1;
+                et = 0;
+            end
+            else if (A == B) begin
+                gt = 0; 
+                lt = 0;
+                et = 1;
+            end
+        end  
+        
+        else if (A[7] == 1 && B[7] == 1) begin
+            if (A > B) begin
+                gt = 1; 
+                lt = 0;
+                et = 0;
+            end
+            else if (A < B) begin
+                gt = 0; 
+                lt = 1;
+                et = 0;
+            end
+            else if (A == B) begin
+                gt = 0; 
+                lt = 0;
+                et = 1;
+            end
+        end  
+        
+        else if (A[7] == 0 && B[7] == 1) begin
+            gt = 1;
+            lt = 0;
             et = 0;
         end
-        else if (A < B) begin
-            gt = 0; 
-            lt = 1; 
+        
+        else if (A[7] == 1 && B[7] == 0) begin
+            gt = 0;
+            lt = 1;
             et = 0;
-        end
-        else if (A == B) begin
-            gt = 0; 
-            lt = 0; 
-            et = 1;
         end
         
     end
-
+    
 endmodule
--- 11) 8 bit comparator/comparator_8bit_tb.v
+++ 11) 8 bit comparator/comparator_8bit_tb.v
@@ -4,23 +4,23 @@ module comparator_8bit_tb();
     reg [7:0] A, B;
     wire gt, lt, et;
     
-    comparator_8bit uut (.A(A), .B(B), .gt(gt), .lt(lt), .et(et));
+    comparator_8bitt uut (.A(A), .B(B), .gt(gt), .lt(lt), .et(et));
     
     initial begin
         // testing et
         A = 8'd25; B = 8'd25;
-        #10; A = 8'd255; B = 8'd255;
-        #10; A = 8'd58; B = 8'd58;
+        #10; A = -8'd55; B = -8'd55;
+        #10; A = 8'd77; B = 8'd77;
         
         // testing gt
         #10; A = 8'd78; B = 8'd25;
-        #10; A = 8'd255; B = 8'd20;
-        #10; A = 8'd58; B = 8'd8;
+        #10; A = 8'd25; B = -8'd40;
+        #10; A = -8'd8; B = -8'd58;
         
         // testing lt
         #10; A = 8'd78; B = 8'd125;
-        #10; A = 8'd25; B = 8'd200;
-        #10; A = 8'd58; B = 8'd89;
+        #10; A = -8'd125; B = 8'd20;
+        #10; A = -8'd58; B = -8'd29;
         
         #10; $finish;
     end
