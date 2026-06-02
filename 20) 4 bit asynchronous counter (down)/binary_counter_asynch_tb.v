0fa3fc2178fc901cfd74d21cafd5fba024dd9c9e
himanigoel0 <24110134@iitgn.ac.in>
2026-06-02 07:32:01 +0530

    Add files via upload

---
+++ 20) 4 bit asynchronous counter (down)/binary_counter_asynch_tb.v
@@ -0,0 +1,21 @@
+`timescale 1ns / 1ps
+
+module binary_counter_asynch_tb();
+    reg clk, rst;
+    wire [3:0] Q;
+    
+    binary_counter_asynch uut (.clk(clk), .rst(rst), .Q(Q));
+    
+    initial clk = 0;
+    always #5 clk = ~clk;
+    
+    initial begin
+        rst = 1;
+        #10; rst = 0; 
+        #100; rst = 1;
+        #10; rst = 0;
+        #200; rst = 1;
+        #10; rst = 0;
+    end
+    
+endmodule
