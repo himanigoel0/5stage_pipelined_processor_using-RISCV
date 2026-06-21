977f5724d0ba1dc58f7cca70e2c6218acebef740
himanigoel0 <24110134@iitgn.ac.in>
2026-06-21 17:25:49 +0530

    Add files via upload

---
+++ 83) array sum program from memory/dmem.v
@@ -0,0 +1,43 @@
+`timescale 1ns / 1ps
+
+module dmem(
+    input clk,
+    input memwrite,
+    input memread,
+    input [31:0] addr,
+    input [31:0] write_data,
+
+    output reg [31:0] read_data
+);
+
+    reg [31:0] mem [0:255];
+    integer i;
+
+    initial begin
+        for(i=0; i<256; i=i+1)
+            mem[i] = 0;
+//        mem[0] = 25;
+    end
+    
+    initial begin
+        mem[0] = 5;
+        mem[1] = 10;
+        mem[2] = 15;
+        mem[3] = 20;
+    end
+
+    // SW
+    always @(posedge clk) begin
+        if(memwrite)
+            mem[addr] <= write_data;
+    end
+
+    // LW
+    always @(*) begin
+        if(memread)
+            read_data = mem[addr];
+        else
+            read_data = 32'b0;
+    end
+
+endmodule
